1d9e13922808e5078a8f0ce3459007503a0a3431
Marina <marina-a-zhukova@yandex.ru>
2026-04-05 19:14:15 +0300

    строковое представление модели

--- src/services/models.py
+++ src/services/models.py
@@ -7,3 +7,6 @@ class Services(models.Model):
     title = models.CharField(verbose_name="Название", max_length=200)
     description = models.TextField(verbose_name="Описание", max_length=1000)
     cost = models.DecimalField(verbose_name="Стоимость", max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal("0.01"))])
+
+    def __str__(self):
+        return self.title
