85f9d5e4d1b958d2e6b7f9125c566d2dbbaad3a4
Marina <marina-a-zhukova@yandex.ru>
2026-04-11 16:27:17 +0300

    тестирование представлений (просмотр списка услуг, просмотр деталей услуги, создание новой услуги)

---
+++ src/services/tests/tests_view.py
@@ -0,0 +1,153 @@
+from decimal import Decimal
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+from ..models import Services
+
+# Create your tests here.
+class TestServicesListView(TestCase):
+    """
+    Тесты для представленния ServicesList.
+    """
+    def setUp(self) -> None:
+        self.service_1 = Services.objects.create(
+            title="Title",
+            description="descrip",
+            cost=Decimal("500.00"),
+        )
+        self.service_2 = Services.objects.create(
+            title="Title 2",
+            description="descrip 2",
+            cost=Decimal("1500.00"),
+        )
+        self.user = User.objects.create_user(username="test_user", password="pass")
+        con_type = ContentType.objects.get_for_model(Services)
+        self.permis = Permission.objects.get(
+            codename="view_services",
+            content_type=con_type,
+        )
+        self.url = reverse("services:services_list")
+
+    def test_get_services_list(self):
+        "Проверка получения списка услуг."
+        self.user.user_permissions.add(self.permis)
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 200)
+        self.assertQuerySetEqual(respons.context["services"], [self.service_2, self.service_1], ordered=False)
+
+    def test_get_services_list_unauthorized(self):
+        "Проверка перенаправления пользователя на страницу входа."
+        respons = self.client.get(self.url)
+        self.assertEqual(respons.status_code, 302)
+        self.assertRedirects(respons, f"/admin/login/?next={self.url}")
+
+    def test_get_services_list_permis(self):
+        "Проверка, что пользователь без разрешения view_services не может просмотреть список услуг."
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 403)
+
+
+class TestServicesDetailView(TestCase):
+    """
+    Тесты для представленния ServicesDetail.
+    """
+    def setUp(self) -> None:
+        self.service = Services.objects.create(
+            title="Title",
+            description="descrip",
+            cost=Decimal("500.00"),
+        )
+        self.user = User.objects.create_user(username="test_user", password="pass")
+        con_type = ContentType.objects.get_for_model(Services)
+        self.permis = Permission.objects.get(
+            codename="view_services",
+            content_type=con_type,
+        )
+        self.url = reverse("services:services_detail", args=[self.service.pk])
+
+    def test_get_services_detail(self):
+        "Проверка получения страницы деталей услуги."
+        self.user.user_permissions.add(self.permis)
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 200)
+        self.assertTemplateUsed(respons, "services/services_detail.html")
+        self.assertEqual(respons.context["object"], self.service)
+
+    def test_get_services_detail_unauthorized(self):
+        "Проверка перенаправления пользователя на страницу входа."
+        respons = self.client.get(self.url)
+        self.assertEqual(respons.status_code, 302)
+        self.assertRedirects(respons, f"/admin/login/?next={self.url}")
+
+    def test_get_services_detail_permis(self):
+        "Проверка, что пользователь без разрешения view_services не может просмотреть список услуг."
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 403)
+
+
+class TestServicesCreateView(TestCase):
+    """
+    Тесты для представленния ServicesCreate.
+    """
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username="test_user", password="pass")
+        con_type = ContentType.objects.get_for_model(Services)
+        self.permis_add = Permission.objects.get(
+            codename="add_services",
+            content_type=con_type,
+        )
+        self.permis_view = Permission.objects.get(
+            codename="view_services",
+            content_type=con_type,
+        )
+        self.url = reverse("services:services_create")
+
+    def test_get_services_create(self):
+        "Проверка получения формы для создания услуги."
+        self.user.user_permissions.add(self.permis_add, self.permis_view)
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 200)
+        self.assertTemplateUsed(respons, "services/services_create.html")
+
+    def test_post_services_create(self):
+        "Проверка получения списка услуг."
+        self.user.user_permissions.add(self.permis_add, self.permis_view)
+        self.client.force_login(self.user)
+        data = {
+            "title": "Test Create",
+            "description": "Test create",
+            "cost": Decimal("500.00"),
+        }
+        respons = self.client.post(self.url, data=data, format="json")
+
+        self.assertEqual(respons.status_code, 302)
+
+        service = Services.objects.first()
+        self.assertRedirects(respons, reverse("services:services_detail", args=[service.pk]))
+
+    def test_get_services_create_unauthorized(self):
+        "Проверка перенаправления пользователя на страницу входа."
+        respons = self.client.get(self.url)
+        self.assertEqual(respons.status_code, 302)
+        self.assertRedirects(respons, f"/admin/login/?next={self.url}")
+
+    def test_get_services_creat_permis(self):
+        "Проверка, что пользователь без разрешения add_services не может создать новую услугу."
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 403)
--- src/services/views.py
+++ src/services/views.py
@@ -1,45 +1,51 @@
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from .models import Services
 from .form import ServicesForm
 
 # Create your views here.
-class ServicesList(PermissionRequiredMixin, ListView):
-    """
-    Отображение списка услуг
-    """
+class ServicesList(PermissionRequiredMixin, LoginRequiredMixin, ListView):
+    "Отображение списка услуг"
     permission_required = ["services.view_services"]
+    login_url = "/admin/login/"
     queryset = Services.objects.all()
     context_object_name = "services"
     # paginate_by = 10
+    # ordering = ["created_at"]
 
 
-class ServicesCreate(CreateView):
+class ServicesCreate(PermissionRequiredMixin, LoginRequiredMixin, CreateView):
     """
     Создание новой услуги
     """
+    permission_required = ["services.add_services", "services.view_services"]
+    login_url = "/admin/login/"
     model = Services
     form_class = ServicesForm
     template_name = "services/services_create.html"
 
     def get_success_url(self) -> str:
-        return reverse_lazy("services_detail", kwargs={"pk": self.object.pk})
+        return reverse_lazy("services:services_detail", kwargs={"pk": self.object.pk})
 
 
-class ServicesDetail(DetailView):
+class ServicesDetail(PermissionRequiredMixin, LoginRequiredMixin, DetailView):
     """
     Информация о выбранной услуге
     """
+    permission_required = ["services.view_services"]
+    login_url = "/admin/login/"
     model = Services
     template_name = "services/services_detail.html"
 
 
-class ServicesUpdate(UpdateView):
+class ServicesUpdate(PermissionRequiredMixin, LoginRequiredMixin, UpdateView):
     """
     Редактирование услуги
     """
+    permission_required = ["services.change_services"]
+    login_url = "/admin/login/"
     model = Services
     form_class = ServicesForm
     template_name = "services/services_edit.html"
@@ -48,10 +54,12 @@ class ServicesUpdate(UpdateView):
         return reverse_lazy("services_detail", kwargs={"pk": self.object.pk})
 
 
-class ServicesDelete(DeleteView):
+class ServicesDelete(PermissionRequiredMixin, LoginRequiredMixin, DeleteView):
     """
     Удаление услуги
     """
+    permission_required = ["services.delete_services"]
+    login_url = "/admin/login/"
     model = Services
     template_name = "services/services_delete.html"
     success_url = reverse_lazy("services_list")
