bda9ee05904da43c0ebdf82de7658b9875dcc393
Marina <marina-a-zhukova@yandex.ru>
2026-04-15 13:15:41 +0300

    тестирование AdvertisementCreate

---
+++ src/ad_campaign/tests/tests_views.py
@@ -0,0 +1,139 @@
+from decimal import Decimal
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
+
+from ..models import Advertisement
+from ...services.models import Services
+
+# Create your tests here.
+class TestAdvertisementListView(TestCase):
+    """
+    Тесты для представленния AdvertisementList.
+    """
+    def setUp(self) -> None:
+        self.service_1 = Services.objects.create(
+            title="Title",
+            description="descrip",
+            cost=Decimal("500.00"),
+        )
+        self.service_2 = Services.objects.create(
+            title="Title 2",
+            description="descrip 2",
+            cost=Decimal("1500.00"),
+        )
+        self.ad_campaign_1 = Advertisement.objects.create(
+            title="Test ad camp",
+            service=self.service_1,
+            promotion_channel="Test 1",
+            budget=Decimal("500.00"),
+        )
+        self.ad_campaign_2 = Advertisement.objects.create(
+            title="Test 2",
+            service=self.service_2,
+            promotion_channel="Test 2 ad camp",
+            budget=Decimal("5000.00"),
+        )
+        self.user = User.objects.create_user(username="test_user", password="pass")
+        con_type = ContentType.objects.get_for_model(Advertisement)
+        self.permis = Permission.objects.get(
+            codename="view_advertisement",
+            content_type=con_type,
+        )
+        self.url = reverse("ad_campaign:ad_list")
+
+    def test_get_advertisement_list(self):
+        "Проверка получения списка рекламных кампаний."
+        self.user.user_permissions.add(self.permis)
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 200)
+        self.assertQuerySetEqual(
+            respons.context["ads"], [self.ad_campaign_1, self.ad_campaign_2], ordered=False
+        )
+
+    def test_get_advertisement_list_unauthorized(self):
+        "Проверка перенаправления пользователя на страницу входа."
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 302)
+        self.assertRedirects(respons, f"/admin/login/?next={self.url}")
+
+    def test_get_advertisement_list_permis(self):
+        "Проверка, что пользователь без разрешения view_advertisement не может просмотреть список рекламных кампаний."
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 403)
+
+
+class TestAdvertisementCreate(TestCase):
+    """
+    Тесты для представленния AdvertisementCreate.
+    """
+    def setUp(self) -> None:
+        self.service = Services.objects.create(
+            title="Title",
+            description="descrip",
+            cost=Decimal("500.00"),
+        )
+        self.user = User.objects.create_user(username="test_user", password="pass")
+        con_type = ContentType.objects.get_for_model(Advertisement)
+        self.permis_add = Permission.objects.get(
+            codename="add_advertisement",
+            content_type=con_type,
+        )
+        self.permis_view = Permission.objects.get(
+            codename="view_advertisement",
+            content_type=con_type,
+        )
+        self.url = reverse("ad_campaign:ad_create")
+
+    def test_get_advertisement_create(self):
+        "Проверка получения формы для создания рекламной кампании."
+        self.user.user_permissions.add(self.permis_add)
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 200)
+        self.assertTemplateUsed(respons, "ad_campaign/ads_create.html")
+
+    def test_post_advertisement_create(self):
+        "Проверка создания новой рекламной кампании."
+        self.user.user_permissions.add(self.permis_add, self.permis_view)
+        self.client.force_login(self.user)
+        data = {
+            "title": "Test ad camp",
+            "service": self.service.pk,
+            "promotion_channel": "Test 1",
+            "budget": Decimal("500.00"),
+        }
+        respons = self.client.post(self.url, data=data, format="json")
+
+        self.assertEqual(respons.status_code, 302)
+
+        advertisement = Advertisement.objects.first()
+        self.assertRedirects(
+            respons, reverse("ad_campaign:ad_list")
+        )
+
+    def test_get_advertisement_create_unauthorized(self):
+        "Проверка перенаправления пользователя на страницу входа."
+        respons = self.client.get(self.url)
+        self.assertEqual(respons.status_code, 302)
+        self.assertRedirects(respons, f"/admin/login/?next={self.url}")
+
+    def test_get_advertisement_creat_permis(self):
+        "Проверка, что пользователь без разрешения add_advertisement не может создать новую рекламную кампанию."
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 403)
+
+
+# class TestAdvertisementDetail(TestCase):
+#     """
+
+#     """
