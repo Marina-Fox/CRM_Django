d19fdc27af36a4a1207481f281db05cd03a93aaa
Marina <marina-a-zhukova@yandex.ru>
2026-04-23 16:56:14 +0300

    view для добавления потенциального клиента

--- src/clients/urls.py
+++ src/clients/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 
-from .views import LeadList, LeadDetail
+from .views import LeadList, LeadDetail, LeadCreate
 
 app_name = "clients"
 
 urlpatterns = [
     path("", LeadList.as_view(), name="clients_list"),
-    # path("new/", AdvertisementCreate.as_view(), name="ad_create"),
+    path("new/", LeadCreate.as_view(), name="clients_create"),
     path("<int:pk>/", LeadDetail.as_view(), name="clients_detail"),
     # path("<int:pk>/edit/", AdvertisementUpdate.as_view(), name="ad_update"),
     # path("<int:pk>/delete/", AdvertisementDelete.as_view(), name="ad_delete"),
--- src/clients/views.py
+++ src/clients/views.py
@@ -9,11 +9,15 @@ from django.views.generic import (
 )
 
 from .models import Lead
+from .form import LeadForm
+
+
 # Create your views here.
 class LeadList(PermissionRequiredMixin, LoginRequiredMixin, ListView):
     """
     Отображение списка потенциальных клиентов.
     """
+
     permission_required = ["clients.view_lead"]
     login_url = "/admin/login/"
     queryset = Lead.objects.all()
@@ -25,7 +29,23 @@ class LeadDetail(PermissionRequiredMixin, LoginRequiredMixin, DetailView):
     """
     Информация о потенциальном клиенте.
     """
+
     permission_required = ["clients.view_lead"]
     login_url = "/admin/login/"
     model = Lead
     template_name = "clients/leads_detail.html"
+
+
+class LeadCreate(PermissionRequiredMixin, LoginRequiredMixin, CreateView):
+    """
+    Создание нового потенциального клиента.
+    """
+
+    permission_required = ["clients.add_lead"]
+    login_url = "/admin/login/"
+    model = Lead
+    form_class = LeadForm
+    template_name = "clients/leads_create.html"
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("clients:clients_create")
