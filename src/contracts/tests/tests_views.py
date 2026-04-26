d457d5a4f4ef1c87aa52da5fbbb81cc7274e9722
Marina <marina-a-zhukova@yandex.ru>
2026-04-26 21:23:52 +0300

    тестирование ContractsDelete

--- src/contracts/tests/tests_views.py
+++ src/contracts/tests/tests_views.py
@@ -299,3 +299,71 @@ class TestContractsUpdate(TestCase):
         respons = self.client.get(self.url)
 
         self.assertEqual(respons.status_code, 403)
+
+
+class TestContractsDelete(TestCase):
+    """
+    Тестирование представления ContractsDelete.
+    """
+
+    def setUp(self) -> None:
+        self.service = Services.objects.create(
+            title="Title",
+            description="descrip",
+            cost=Decimal("500.00"),
+        )
+        self.file_contr = SimpleUploadedFile(
+            name="test 1.pdf",
+            content=b"test_contract",
+            content_type="application/pdf",
+        )
+        self.end_date = date.today() + timedelta(days=30)
+        self.contract = Contracts.objects.create(
+            title="Test Contr 1",
+            service=self.service,
+            file_doc=self.file_contr,
+            end_date=self.end_date,
+            cost=Decimal("500000.00"),
+        )
+        self.user = User.objects.create_user(username="test_user", password="pass")
+        con_type = ContentType.objects.get_for_model(Contracts)
+        self.permis_view = Permission.objects.get(
+            codename="view_contracts",
+            content_type=con_type,
+        )
+        self.permis_del = Permission.objects.get(
+            codename="delete_contracts",
+            content_type=con_type,
+        )
+        self.url = reverse("contracts:contracts_delete", args=[self.contract.pk])
+
+    def test_get_contracts_delete(self):
+        "Проверка получения формы для удаления контракта."
+        self.user.user_permissions.add(self.permis_del)
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 200)
+        self.assertTemplateUsed(respons, "contracts/contracts_delete.html")
+
+    def test_delete_contracts(self):
+        "Проверка удаления контракта."
+        self.user.user_permissions.add(self.permis_del, self.permis_view)
+        self.client.force_login(self.user)
+        respons = self.client.delete(self.url)
+
+        self.assertEqual(respons.status_code, 302)
+        self.assertRedirects(respons, reverse("contracts:contracts_list"))
+
+    def test_get_contracts_delete_unauthorized(self):
+        "Проверка перенаправления пользователя на страницу входа."
+        respons = self.client.get(self.url)
+        self.assertEqual(respons.status_code, 302)
+        self.assertRedirects(respons, f"/admin/login/?next={self.url}")
+
+    def test_get_contracts_delete_permis(self):
+        "Проверка, что пользователь без разрешения delete_contracts не может удалить контракт."
+        self.client.force_login(self.user)
+        respons = self.client.get(self.url)
+
+        self.assertEqual(respons.status_code, 403)
