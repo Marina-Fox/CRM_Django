931de19b15a78d0b13173012ccea7f906f465ecc
Marina <marina-a-zhukova@yandex.ru>
2026-04-27 21:45:51 +0300

    представления

--- src/customers/views.py
+++ src/customers/views.py
@@ -9,6 +9,7 @@ from django.views.generic import (
 )
 
 from .models import Customers
+from .form import CustomersForm
 
 
 # Create your views here.
@@ -21,3 +22,56 @@ class CustomersList(PermissionRequiredMixin, LoginRequiredMixin, ListView):
     queryset = Customers.objects.all()
     template_name = "customers/customers_list.html"
     context_object_name = "customers"
+
+
+class CustomersDetail(PermissionRequiredMixin, LoginRequiredMixin, DetailView):
+    """
+    Информация о потенциальном клиенте.
+    """
+
+    permission_required = ["customers.view_customers"]
+    login_url = "/admin/login/"
+    model = Customers
+    template_name = "customers/customers_detail.html"
+
+
+class CustomersCreate(PermissionRequiredMixin, LoginRequiredMixin, CreateView):
+    """
+    Добавление нового активного клиента.
+    """
+
+    permission_required = ["customers.add_customers"]
+    login_url = "/admin/login/"
+    model = Customers
+    form_class = CustomersForm
+    template_name = "customers/customers_create.html"
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("customers:customers_detail", args=[self.object.pk])
+
+
+class CustomersUptade(PermissionRequiredMixin, LoginRequiredMixin, UpdateView):
+    """
+    Редактирование активного клиента.
+    """
+
+    permission_required = ["customers.change_customers"]
+    login_url = "/admin/login/"
+    model = Customers
+    form_class = CustomersForm
+    template_name = "customers/customers_edit.html"
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("customers:customers_detail", args=[self.object.pk])
+
+
+class CustomersDelete(PermissionRequiredMixin, LoginRequiredMixin, DeleteView):
+    """
+    Удаление активного клиента.
+    """
+
+    permission_required = ["customers.delete_customers"]
+    login_url = "/admin/login/"
+    model = Customers
+    template_name = "customers/customers_delete.html"
+    success_url = reverse_lazy("customers:customers_list")
