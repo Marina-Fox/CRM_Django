b929d72c5bf1ca2606185d8df5f3b8fb1acb8526
Marina <marina-a-zhukova@yandex.ru>
2026-04-27 21:37:11 +0300

    пути

--- src/customers/urls.py
+++ src/customers/urls.py
@@ -2,18 +2,18 @@ from django.urls import path
 
 from .views import (
     CustomersList,
-    # ContractsDetail,
-    # ContractsCreate,
-    # ContractsUptade,
-    # ContractsDelete,
+    CustomersCreate,
+    CustomersDetail,
+    CustomersUptade,
+    CustomersDelete,
 )
 
 app_name = "customers"
 
 urlpatterns = [
     path("", CustomersList.as_view(), name="customers_list"),
-    # path("new/", ContractsCreate.as_view(), name="contracts_create"),
-    # path("<int:pk>/", ContractsDetail.as_view(), name="contracts_detail"),
-    # path("<int:pk>/edit/", ContractsUptade.as_view(), name="contracts_update"),
-    # path("<int:pk>/delete/", ContractsDelete.as_view(), name="contracts_delete"),
+    path("new/", CustomersCreate.as_view(), name="customers_create"),
+    path("<int:pk>/", CustomersDetail.as_view(), name="customers_detail"),
+    path("<int:pk>/edit/", CustomersUptade.as_view(), name="customers_update"),
+    path("<int:pk>/delete/", CustomersDelete.as_view(), name="customers_delete"),
 ]
