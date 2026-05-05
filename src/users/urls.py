a5e223281a4ba4231bc3e8c1042a4d36b43163bc
Marina <marina-a-zhukova@yandex.ru>
2026-05-05 18:57:20 +0300

    перенаправление после входа

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
+
+from .views import CustomLoginView
 
 
 app_name = "users"
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="users:login"), name="logout"),
 ]
