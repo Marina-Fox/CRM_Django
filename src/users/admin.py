36b2e4d4c3f4f243629bb91a204e34a9d48384de
Marina <marina-a-zhukova@yandex.ru>
2026-08-10 21:08:18 +0300

    административная панель для администраторов без статуса суперпользователя

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
+admin.site.unregister(User)
+
+@admin.register(User)
+class AdminUser(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    readonly_fields = ("last_login", "date_joined")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "username",
+                    "password",
+                ),
+            },
+        ),
+        (
+            "Персональная информация",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                ),
+            },
+        ),
+        (
+            "Права доступа",
+            {
+                "fields": (
+                    "is_active",
+                    "groups",
+                ),
+            },
+        ),
+        (
+            "Важные даты",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                ),
+            },
+        ),
+    )
